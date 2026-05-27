7011ddd0a44001323cf767e14eb2557f97ac2418
Huahai Yang <huahai.yang@gmail.com>
2026-05-26 17:08:24 -0700

    fix test-jar build

--- test-jar/build.clj
+++ test-jar/build.clj
@@ -5,8 +5,23 @@
 (def class-dir "target/classes")
 (def uber-file "target/test-jar.jar")
 
+(defn- datalevin-version []
+  (or (second (re-find #"def version \"([0-9\.]+)\"" (slurp "../project.clj")))
+      (throw (ex-info "Datalevin version not found in ../project.clj" {}))))
+
+(defn- datalevin-jar []
+  (let [jar (format "../target/datalevin-%s.jar" (datalevin-version))]
+    (when-not (.exists (b/resolve-path jar))
+      (throw (ex-info (str "Datalevin jar not found: " jar)
+                      {:jar jar})))
+    jar))
+
 ;; delay to defer side effects (artifact downloads)
-(def basis (delay (b/create-basis {:project "deps.edn"})))
+(def basis
+  (delay
+    (b/create-basis
+      {:project "deps.edn"
+       :extra   {:deps {'datalevin/datalevin {:local/root (datalevin-jar)}}}})))
 
 (defn clean [_]
   (b/delete {:path "target"}))
